eca17cc520dff04c058433019360a82b856df090
Maurício Souza <mssoliveira1995@gmail.com>
2024-02-19 11:07:45 -0300

    new

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+
+import { ThemeProvider } from '@/components/theme-provider';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="pt-BR">
+			<Head />
+			<body>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<main className="flex min-h-screen flex-col items-center p-24 justify-start">
+						<Main />
+						<NextScript />
+					</main>
+				</ThemeProvider>
+			</body>
+		</Html>
+	);
 }
